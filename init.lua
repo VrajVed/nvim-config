03bd5cffb6df7ca8a902c97f48a75cc5cfb17387
VrajVed <vraj.a.ved@gmail.com>
2026-06-12 21:49:42 +0530

    fixed bug with treesitter

---
+++ init.lua
@@ -0,0 +1,310 @@
+vim.opt.termguicolors = true
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal VIM SETTINGS
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
+
+
+
+-- for indentation
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- for searching
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+
+-- move between windows
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+
+    -- Colorscheme 
+
+    {
+        "folke/tokyonight.nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("tokyonight")
+        end,
+    },
+
+    -- LSP MASON
+
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        }
+    },
+
+    --LUALINE
+
+    {
+       'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup {
+                options = {
+                    icons_enabled = true,
+                    theme = 'auto',
+                    component_separators = { left = '', right = ''},
+                    section_separators = { left = '', right = ''},
+                    disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                    },
+                    ignore_focus = {},
+                    always_divide_middle = true,
+                    always_show_tabline = true,
+                    globalstatus = false,
+                    refresh = {
+                    statusline = 1000,
+                    tabline = 1000,
+                    winbar = 1000,
+                    refresh_time = 16, -- ~60fps
+                    events = {
+                        'WinEnter',
+                        'BufEnter',
+                        'BufWritePost',
+                        'SessionLoadPost',
+                        'FileChangedShellPost',
+                        'VimResized',
+                        'Filetype',
+                        'CursorMoved',
+                        'CursorMovedI',
+                        'ModeChanged',
+                    },
+                    }
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {'filename'},
+                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {'filename'},
+                    lualine_x = {'location'},
+                    lualine_y = {},
+                    lualine_z = {}
+                },
+                tabline = {},
+                winbar = {},
+                inactive_winbar = {},
+                extensions = {}
+                }
+        end,
+    },
+
+
+    -- TYPST PREVIEW
+    {
+        'chomosuke/typst-preview.nvim',
+        lazy = false, -- or ft = 'typst'
+        version = '1.*', -- use the latest stable version
+        opts = {},
+    },
+
+    -- OPENCODE
+    {
+        "nickjvandyke/opencode.nvim",
+        version = "*", -- Latest stable release
+        config = function()
+        ---@type opencode.Opts
+        vim.g.opencode_opts = {
+            -- Your configuration, if any; goto definition on the type or field for details
+        }
+
+        vim.o.autoread = true -- Required for `vim.g.opencode_opts.events.reload`
+
+        -- Recommended/example keymaps
+        vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ") end, { desc = "Ask opencode…" })
+        vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end,       { desc = "Select opencode…" })
+
+        vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
+        vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+
+        vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
+        vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+        end,
+    },
+
+
+    -- WHICH KEY
+
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            preset = "modern", 
+            win = {
+                border = "rounded",
+            },
+            icons = {
+                breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+                separator = "➜", -- symbol used between a key and it's label
+                group = "+", -- symbol prepended to a group
+            }
+        },
+        keys = {
+            {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+
+    -- TREE SITTER
+
+    {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+
+    config = function()
+        require("nvim-treesitter").setup({
+            ensure_installed = {
+                "lua",
+                "python",
+                "javascript",
+                "typescript",
+                "html",
+                "css",
+                "json",
+                "markdown",
+                "go",
+                "bash",
+            },
+
+            highlight = {
+                enable = true,
+            },
+
+            indent = {
+                enable = true,
+            },
+        })
+    end,
+},
+
+    -- TELESCOPE
+
+    {
+        'nvim-telescope/telescope.nvim', version = '*',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- optional but recommended
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        },
+            config = function()
+                local builtin = require("telescope.builtin")
+
+                vim.keymap.set("n", "<leader>ff", builtin.find_files)
+                vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+                vim.keymap.set("n", "<leader>fb", builtin.buffers)
+                vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+            end,
+    },
+
+    -- Neo Tree
+
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons", -- optional, but recommended
+        },
+        lazy = false, -- neo-tree will lazily load itself,
+
+        config = function()
+            require("neo-tree").setup({})
+                -- your configuration goes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+
+                vim.keymap.set(
+                    "n",
+                    "<leader>e",
+                    "<cmd>Neotree toggle<CR>",
+                    { desc = "Tree" }
+                )
+    
+        end,
+        
+    },
+
+
+
+
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "tokyonight" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
